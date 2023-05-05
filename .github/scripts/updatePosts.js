7f33a1dd1439e3bf5a31dfbb3d8e6ae7f2e899fa
Carter Capocaccia <carter.capocaccia@gmail.com>
2023-05-05 13:32:51 -0500

    initial commit for dynamic readme update

--- .github/scripts/updatePosts.js
+++ .github/scripts/updatePosts.js
@@ -2,7 +2,8 @@ const axios = require('axios');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const fs = require('fs')
-const { Octokit, App } = require("octokit");
+const { Octokit } = require("@octokit/rest");
+const { Base64 } = require("js-base64");
 
 axios
   .get("https://www.capocaccia.dev/")
@@ -11,9 +12,24 @@ axios
     const header = dom.window.document.querySelector('[data-testid="hero-header"]');
     const excerpt = dom.window.document.querySelector('[data-testid="hero-post-excerpt"]')
     const content = `${header.textContent} - ${excerpt.textContent}`
-    const octo = new Octokit({auth: process.env['GITHUB_TOKEN']})
+    const octoClient = new Octokit({auth: process.env['GITHUB_TOKEN']})
     const repo = process.env['GITHUB_REPOSITORY']
-    const readMe = octo.readMe(repo)
-    console.log(readMe)
-
+    const readMeContent = fs.readFileSync("./readMe.md", "utf-8");
+    const readMeContentEncoded = Base64.encode(content);
+    octokit.repos.createOrUpdateFileContents({
+      // replace the owner and email with your own details
+      owner: "Capocaccia",
+      repo: "Capocaccia",
+      path: "readMe.md",
+      message: "Updates readme",
+      content: contentEncoded,
+      committer: {
+        name: `Octokit Bot`,
+        email: "carter.capocaccia@gmail.com",
+      },
+      author: {
+        name: "Octokit Bot",
+        email: "carter.capocaccia@gmail.com",
+      },
+    });
    });

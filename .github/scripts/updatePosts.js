684605119a66d6e42d6a0e439a748393469e762c
Carter Capocaccia <carter.capocaccia@gmail.com>
2023-05-05 13:18:57 -0500

    working on env vars

--- .github/scripts/updatePosts.js
+++ .github/scripts/updatePosts.js
@@ -7,13 +7,12 @@ const { Octokit, App } = require("octokit");
 axios
   .get("https://www.capocaccia.dev/")
   .then(function (response) {
-    console.log(process.env)
     const dom = new JSDOM(response.data);
     const header = dom.window.document.querySelector('[data-testid="hero-header"]');
     const excerpt = dom.window.document.querySelector('[data-testid="hero-post-excerpt"]')
     const content = `${header.textContent} - ${excerpt.textContent}`
     const octo = new Octokit({auth: process.env['GITHUB_TOKEN']})
-    const repo = ENV['GITHUB_REPOSITORY']
+    const repo = process.env['GITHUB_REPOSITORY']
     const readMe = octo.readMe(repo)
     console.log(readMe)
 

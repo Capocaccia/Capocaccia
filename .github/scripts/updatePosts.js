2959193b61b7e05781b570439e7e1b9b331b1ccd
Carter Capocaccia <carter.capocaccia@gmail.com>
2023-05-05 13:39:37 -0500

    initial commit for dynamic readme update

--- .github/scripts/updatePosts.js
+++ .github/scripts/updatePosts.js
@@ -12,14 +12,15 @@ axios
     const header = dom.window.document.querySelector('[data-testid="hero-header"]');
     const excerpt = dom.window.document.querySelector('[data-testid="hero-post-excerpt"]')
     const content = `${header.textContent} - ${excerpt.textContent}`
-    const encodedContent = Base64.encode(content);
+    const octoClient = new Octokit({auth: process.env['GITHUB_TOKEN']})
+    const readMeContentEncoded = Base64.encode(content);
     octoClient.repos.createOrUpdateFileContents({
       // replace the owner and email with your own details
       owner: "Capocaccia",
       repo: "Capocaccia",
       path: "README.md",
       message: "Updates readme",
-      content: encodedContent,
+      content: readMeContentEncoded,
       committer: {
         name: `Octokit Bot`,
         email: "carter.capocaccia@gmail.com",
